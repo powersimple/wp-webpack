1b90b652f013c0375ffe961200992259360f0228
Ben Erwin <powersimple@gmail.com>
2018-04-18 11:19:21 -0400

    configs

---
+++ js/components/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1>Hello There!</h1>
+    <blockquote>We humans are alone in this world for a reason. We've murdered and butchered anything that challenged our primacy</blockquote>
+    <p>-Dr. Robert Ford</p>
+  </div>
+</template>
+<script>
+
+export default {
+  created(){
+    console.log('created!');
+  }
+}
+</script>
--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log('WebPack is pretty neat! Suck it, haters.');
+import Vue from 'vue'
+
+import hello from './components/hello.vue'
+
+new Vue({
+  el: '#app',
+  components: {
+    'hello': hello
+  }
+})
